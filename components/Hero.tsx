b23c329288678d4777cd1fa4598c51d469431912
Vladimir Orlov <satancrew.true@gmail.com>
2023-08-07 13:48:46 +0300

    feat: add layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,13 +20,14 @@ const Hero = () => {
           title="Explore Card"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
+          btnType="button"
         />
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
-          <div className="hero__image-overlay" />
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
