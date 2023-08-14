c93ae948f6b6cb66b278a70062f3f9816f102fb2
Vladimir Orlov <86786994+Satancrew@users.noreply.github.com>
2023-08-14 19:18:30 +0300

    feat: add car images from api (#6)

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { CarProps } from '@/types';
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
 import Image from 'next/image';
 import { useState } from 'react';
 import { CarDetails, CustomButton } from '.';
@@ -28,7 +28,7 @@ const CarCard = ({ car }: CarCardProps) => {
       </p>
       <div className="relative w-full h-40 my-3 object-contain">
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="car model"
           fill
           priority
@@ -68,7 +68,11 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
-      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
